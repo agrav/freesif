c464887817334cd1dbc3c4587f185ec7ea598fda
tovop <per.voie@gmail.com>
2020-06-03 13:08:36 +0200

    Enable versioning using Git tags, rendering of README in markdown syntax and limit depenencies by version.

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from codecs import open  # To use a consistent encoding
-from os import path
+import os
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
-    long_description = f.read()
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
+    # package data
     name='freesif',
-    version='0.1',
     description='Get data from Sesam Interface Files',
-    long_description=long_description,
-#    url='https://github.com/agrav/freesif',
+    use_scm_version=True,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data=dict(),
+    python_requires='~=3.7',
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'tables>=3.6,<4',
+        'numpy>=1.17,<2'
+    ],
+    zip_safe=True,
+
+    # meta data
+    long_description=read('README.md'),
+    keywords='sesam structural hydrodynamic',
+    url='https://github.com/agrav/freesif',
     author='Audun Gravdal Johansen',
     author_email='audun.gravdal.johansen@gmail.com',
     license='MIT',
     classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        # 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
-    keywords='sesam structural hydrodynamic',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['tables', 'numpy'],
 )
